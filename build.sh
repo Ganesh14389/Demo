08d1aca670fd1bc39d165872722ffdb37388dc7f
Ganesh14389 <68822653+Ganesh14389@users.noreply.github.com>
2023-09-21 15:01:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-cd ..
-mvn clean install
+git clone https://github.com/hkhcoder/vprofile-project.git
+git checkout main
+JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64"
+ /usr/bin/mvn clean install
+
+
 
